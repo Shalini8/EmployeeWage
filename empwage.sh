c2a2490d01815191461412d29e989ede18514c37
Shalini8 <shalu8mar@gmail.com>
2021-03-12 11:25:28 +0530

    addedd//

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,17 @@
-#! bin/bash
 
-echo "employe wage"
+#! /bin/bash  -x
+ 
+# Constant 
+ IS_PRESENT=1
+
+#Variable
+empCheck=$(( RANDOM % 2))
+
+# == -) -eq / < -)
+if [ $IS_PRESENT -eq $empCheck ]
+then
+  echo "Employee is present"
+else
+  echo "Employee is absent"
+fi 
+
