e53c98186f53f0cb9761b01484e079d091b441dc
Shalini8 <shalu8mar@gmail.com>
2021-03-15 18:56:22 +0530

    cal till workinghrs is reached

---
+++ empWageV7.sh
@@ -0,0 +1,35 @@
+#! /bin/bash  
+ 
+# Constant 
+ IS_PRESENT_FULLTIME=1
+ IS_PRESENT_PARTTIME=2
+ EMP_RATE_PER_HR=20
+MAX_WORKING_DAYS=20  
+MAX_WORKING_HOURS=90
+
+#variable 
+totalWorkingHours=0
+totalWorkingDays=0
+
+ while [ $totalWorkingDays -lt $MAX_WORKING_DAYS ]  &&  [ $totalWorkingHours -lt $MAX_WORKING_HOURS ]
+ do
+#VARIABLE 
+  empCheck=$(( RANDOM % 3))
+     ((totalWorkingDays++))
+
+case $empCheck  in 
+        $IS_PRESENT_FULLTIME )
+ 			empHrs=8;;
+     		$IS_PRESENT_PARTTIME) 
+			empHrs=4 ;;
+    				*)
+      	empHrs=0 ;;
+  esac
+   totalWorkingHours=$((  $totalWorkingHours + $empHrs ))
+      
+done 
+
+salary=$(( $totalWorkingHours * $EMP_RATE_PER_HR ))
+
+ printf "\n"
+
