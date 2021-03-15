cd9066f982227bb287af0d98b4e66350b30d84c8
Shalini8 <shalu8mar@gmail.com>
2021-03-15 19:57:20 +0530

    store-dailywages

---
+++ empWageV9.sh
@@ -0,0 +1,56 @@
+#! /bin/bash -X  
+ 
+# Constant 
+ IS_PRESENT_FULLTIME=1
+ IS_PRESENT_PARTTIME=2
+ EMP_RATE_PER_HR=20
+ MAX_WORKING_DAYS=20  
+ MAX_WORKING_HOURS=20
+
+#variable 
+totalWorkingHours=0
+totalWorkingDays=0
+ 
+declare -a dailywages
+
+function getWorkingHours(){
+   local empCheck=$1
+	local empHrs=0
+	case $empCheck  in
+        $IS_PRESENT_FULLTIME )
+         empHrs=8;;
+         $IS_PRESENT_PARTTIME)
+         empHrs=4 ;;
+       *)
+         empHrs=0 ;;
+  esac
+echo $empHrs   
+
+}
+
+function calwageForDay(){
+	local empHrs=$1 
+	echo $(( $EMP_RATE_PER_HR * $empHrs ))
+}
+
+while [ $totalWorkingDays -lt $MAX_WORKING_DAYS ]  && [ $totalWorkingHours -lt  $MAX_WORKING_HOURS ]
+ do
+#VARIABLE 
+  empCheck=$(( RANDOM % 3 ))
+     totalWorkingDays=$(( $totalWorkingDays+1 ))
+
+ empHrs="$( getWorkingHours $empCheck )"
+
+ totalWorkingHours=$(($totalWorkingHours + $empHrs))
+      
+ dailywages[$totalWorkingDays]="$( calwageForDay $empHrs)"
+
+done 
+echo ${!dailywages[*]}
+echo ${dailywages[*]}
+
+salary=$(( $totalWorkingHours * $EMP_RATE_PER_HR ))
+
+ 
+
+
